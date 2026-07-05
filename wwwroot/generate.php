d7aace4eb39dd23ef21a5f99667351ed4ee46040
Fernando Herrero <ininflamable@gmail.com>
2026-07-05 05:04:59 +0200

    move to wwwroot

--- wwwroot/generate.php
+++ wwwroot/generate.php
@@ -1,7 +1,7 @@
 <?php
 	declare(strict_types=1);
 
-	require __DIR__ . '/vendor/autoload.php';
+	require dirname(__DIR__) . '/vendor/autoload.php';
 
 	use phpseclib3\Crypt\Common\PrivateKey;
 	use phpseclib3\Crypt\EC;
