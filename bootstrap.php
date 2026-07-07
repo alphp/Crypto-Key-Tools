5891672367de0e47494944f636649b259ba825ec
Fernando Herrero <ininflamable@gmail.com>
2026-07-07 11:17:28 +0200

    feat(bootstrap): normalize agnostic routing and secure file resolution

--- bootstrap.php
+++ bootstrap.php
@@ -5,8 +5,20 @@
 
 	define('DS', DIRECTORY_SEPARATOR);
 	define('ROOT', __DIR__);
+	define('WWWROOT', __DIR__ . DS . 'wwwroot' . DS);
 	define('REQUEST_BASE', rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\') . '/');
 	define('TEMPLATES', __DIR__ . DS . 'templates' . DS);
+
+	$request_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?: '';
+	$request_file = str_starts_with($request_path, REQUEST_BASE) ? substr($request_path, strlen(REQUEST_BASE)) : $request_path;
+
+	$request_file = ($request_file === 'index') ? '' : $request_file;
+	$dotfile = ((pathinfo($request_file, PATHINFO_FILENAME) === '') and (pathinfo($request_file, PATHINFO_EXTENSION) !== ''));
+
+	if ($request_file and !$dotfile and basename($request_file . '.php') !== '.php' and is_file(WWWROOT . $request_file . '.php')) {
+		include WWWROOT . $request_file . '.php';
+		exit;
+	}
 
 	use Alphp\CryptoKeyTools\View\KeyFactoryConvertView;
 	use Alphp\CryptoKeyTools\View\KeyFactoryGenerateView;
@@ -21,6 +33,5 @@
 		KeyFactoryConvertView::REQUEST_URI  => KeyFactoryConvertView::class,
 	];
 
-	$request_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?: '';
 	($router[$request_path] ?? Error404View::class)::init()->response();
 
