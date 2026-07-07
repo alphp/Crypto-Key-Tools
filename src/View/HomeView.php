66b7e1c7dbbfb42d613dbc384de46c2a6002e8ee
Fernando Herrero <ininflamable@gmail.com>
2026-07-07 11:29:58 +0200

    feat(security): delegate CSP management to application layer and fix CDN source map blocks

--- src/View/HomeView.php
+++ src/View/HomeView.php
@@ -7,6 +7,8 @@
 		public const REQUEST_URI = REQUEST_BASE;
 
 		public function response () : void {
+			header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdnjs.cloudflare.com; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; font-src 'self' https://cdnjs.cloudflare.com; img-src 'self' data:; connect-src 'self' https://cdnjs.cloudflare.com;");
+
 			$generate = KeyFactoryGenerateView::REQUEST_URI;
 			$convert = KeyFactoryConvertView::REQUEST_URI;
 
